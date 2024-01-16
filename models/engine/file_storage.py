cfaee9cf2297d7afb2b639ecc0ee6bb2734581d9
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2024-01-16 12:32:25 +0300

    next

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,18 +42,17 @@ class FileStorage:
         Args:
             obj (BaseModel): The object to be added.
         """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
         """
         Serializes __objects to the JSON file (__file_path).
         """
-        ser_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            ser_dict[key] = obj.to_dict()
+        ser_dict = FileStorage.__objects
+        obj_dict = {obj: ser_dict[obj].to_dict() for obj in ser_dict.keys()}
         with open(FileStorage.__file_path, 'w') as file:
-            json.dump(ser_dict, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects.
@@ -64,13 +63,9 @@ class FileStorage:
         try:
             with open(FileStorage.__file_path) as file:
                 obj_dict = json.load(file)
-                for obj_id, obj_data in obj_dict.items():
-                    class_name = obj_data["__class__"]
-                    del obj_data["__class__"]
-                    try:
-                        cls = eval(class_name)
-                    except NameError:
-                        cls = BaseModel
-                    self.new(cls(**obj_data))
+                for obj_id in obj_dict.values():
+                    class_name = obj_id["__class__"]
+                    del obj_id["__class__"]
+                    self.new(eval(class_name)(**obj_id))
         except FileNotFoundError:
             return
